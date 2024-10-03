3a1e0a190689d96e55a98cdeacdf7cff360d65c2
Rikke Kristiansen <rikke.kristiansen@hotmail.com>
2024-10-03 14:43:08 +0200

    first commit

---
+++ tr-plugin/plugin.php
@@ -0,0 +1,7 @@
+<?php
+/* 
+Plugin Name: Tr plugin
+Description: Plugin to website Tech rebels
+Author: Rikke
+Version 1.0.0 
+*/
---
+++ tr-theme/functions.php
@@ -0,0 +1,34 @@
+<?php
+// Funktion för att ladda in React-bundle från Vite
+function load_react_app() {
+
+    if (defined('WP_DEBUG') && WP_DEBUG) {
+   
+        echo '<script type="module" src="http://localhost:5173/src/main.tsx"></script>';
+    } else {
+        // Ladda den byggda React-appens JavaScript-fil i produktion
+        wp_enqueue_script(
+            'react-app',
+            get_template_directory_uri() . '/headlessfront/dist/assets/index.js', // Sökväg till din Vite-byggda bundle
+            array(), 
+            null, 
+            true 
+        );
+    }
+}
+add_action('wp_enqueue_scripts', 'load_react_app');
+
+function custom_api_route() {
+    register_rest_route('myplugin/v1', '/data/', array(
+        'methods' => 'GET',
+        'callback' => 'custom_api_callback',
+    ));
+}
+
+function custom_api_callback() {
+    return new WP_REST_Response('Hello from the API!', 200);
+}
+
+add_action('rest_api_init', 'custom_api_route');
+
+?>
