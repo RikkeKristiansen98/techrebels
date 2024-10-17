6d9bd0dc59802344c31cdcf9e92cc6f0fde6bb76
jafar-hussein <jafarhussein6@gmail.com>
2024-10-17 10:25:11 +0200

    fixade

--- tr-theme/functions.php
+++ tr-theme/functions.php
@@ -2,16 +2,45 @@
 function load_react_app() {
 
     if (defined('WP_DEBUG') && WP_DEBUG) {
-   
+
         echo '<script type="module" src="http://localhost:5173/src/main.tsx"></script>';
     } else {
-        wp_enqueue_script(
-            'react-app',
-            get_template_directory_uri() . '/headlessfront/dist/assets/index.js', 
-            array(), 
-            null, 
-            true 
-        );
+        // Produktionsläge - dynamisk sökning av byggfilerna
+        $assets_dir = get_template_directory() . '/frontend/dist/assets/';
+        $assets_url = get_template_directory_uri() . '/frontend/dist/assets/';
+
+        // Hitta rätt JS- och CSS-filer dynamiskt
+        $js_file = '';
+        $css_file = '';
+
+        foreach (glob($assets_dir . 'index-*.js') as $file) {
+            $js_file = basename($file);
+        }
+
+        foreach (glob($assets_dir . 'index-*.css') as $file) {
+            $css_file = basename($file);
+        }
+
+        // Enqueue JavaScript-filen
+        if ($js_file) {
+            wp_enqueue_script(
+                'react-app',
+                $assets_url . $js_file,
+                array(),
+                filemtime($assets_dir . $js_file), // Cache-busting med filens senaste ändringstid
+                true // Ladda i footern
+            );
+        }
+
+        // Enqueue CSS-filen
+        if ($css_file) {
+            wp_enqueue_style(
+                'react-app-css',
+                $assets_url . $css_file,
+                array(),
+                filemtime($assets_dir . $css_file) // Cache-busting med filens senaste ändringstid
+            );
+        }
     }
 }
 add_action('wp_enqueue_scripts', 'load_react_app');
