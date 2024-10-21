faede56a16701fdb5a50c2d1a0ab068af5b38759
Rikke Kristiansen <rikke.kristiansen@hotmail.com>
2024-10-21 10:21:17 +0200

    -

--- tr-theme/frontend/src/components/Footer.jsx
+++ tr-theme/frontend/src/components/Footer.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import '../styles/footer.css';
 import { NavLink } from 'react-router-dom';
+import footerbild from '../img/footer.png'; 
 
-/* tillfällig footer*/
 const Footer = () => {
   return (
-    <footer className="footer">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-  <path fill="#3f5438" fill-opacity="1" d="M0,128L60,144C120,160,240,192,360,197.3C480,203,600,181,720,154.7C840,128,960,96,1080,106.7C1200,117,1320,171,1380,197.3L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
-</svg>
+    <footer className="footer" style={{ backgroundImage: `url(${footerbild})` }}>
       <div className="footer-content">
         <div className="newsletter">
           <h3>Prenumerera på nyhetsbrev</h3>
