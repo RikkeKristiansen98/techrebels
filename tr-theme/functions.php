67f3cae1c0ca949f62953564a211cfd1e6737fb9
Rikke Kristiansen <rikke.kristiansen@hotmail.com>
2024-11-20 09:37:10 +0100

    ändrat för att det skall visas på sidan

--- tr-theme/functions.php
+++ tr-theme/functions.php
@@ -1,82 +1,12 @@
 <?php
 function load_react_app() {
-
-    if (defined('WP_DEBUG') && WP_DEBUG) {
-
-        echo '<script type="module" src="http://localhost:5173/src/main.tsx"></script>';
-    } else {
-        // Produktionsläge - dynamisk sökning av byggfilerna
-        $assets_dir = get_template_directory() . '/frontend/dist/assets/';
-        $assets_url = get_template_directory_uri() . '/frontend/dist/assets/';
-
-        // Hitta rätt JS- och CSS-filer dynamiskt
-        $js_file = '';
-        $css_file = '';
-
-        foreach (glob($assets_dir . 'index-*.js') as $file) {
-            $js_file = basename($file);
-        }
-
-        foreach (glob($assets_dir . 'index-*.css') as $file) {
-            $css_file = basename($file);
-        }
-
-        // Enqueue JavaScript-filen
-        if ($js_file) {
-            wp_enqueue_script(
-                'react-app',
-                $assets_url . $js_file,
-                array(),
-                filemtime($assets_dir . $js_file), // Cache-busting med filens senaste ändringstid
-                true // Ladda i footern
-            );
-        }
-
-        // Enqueue CSS-filen
-        if ($css_file) {
-            wp_enqueue_style(
-                'react-app-css',
-                $assets_url . $css_file,
-                array(),
-                filemtime($assets_dir . $css_file) // Cache-busting med filens senaste ändringstid
-            );
-        }
-    }
+    $assets_url = get_template_directory_uri() . '/frontend/dist/assets/';
+    
+    // JavaScript och CSS-filer som kommer från den byggda versionen
+    wp_enqueue_script('react-app', $assets_url . 'index-CbDfEAQT.js', array(), null, true);
+    wp_enqueue_style('react-app-css', $assets_url . 'index-D4nLGXzL.css', array(), null);
 }
 add_action('wp_enqueue_scripts', 'load_react_app');
-
-function custom_api_route() {
-    register_rest_route('myplugin/v1', '/data/', array(
-        'methods' => 'GET',
-        'callback' => 'custom_api_callback',
-    ));
-}
-
-function custom_api_callback() {
-    return new WP_REST_Response('Hello from the API!', 200);
-}
-
-add_action('rest_api_init', 'custom_api_route');
-
-/* 
-function create_tips_post_type() {
-    register_post_type('tips',
-        array(
-            'labels' => array(
-                'name' => __('Tips'),
-                'singular_name' => __('Tips')
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'supports' => array('title', 'editor', 'thumbnail'),
-            'show_in_rest' => true, // Detta gör att du kan hämta tips via REST API
-        )
-    );
-}
-add_action('init', 'create_tips_post_type');
-
-*/
-
 ?>
 
 
