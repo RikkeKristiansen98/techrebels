7a25f8ce5a41330b90652330dcb0699117c2ceed
varamara <mikahvaarama@gmail.com>
2024-11-27 13:49:00 +0100

    Add Tailwind CSS configuration and integrate into project styles

---
+++ tr-theme/frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.css", // Inkluderar din index.css i `src`
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
