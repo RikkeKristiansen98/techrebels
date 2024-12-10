3ccb995bcf47472d7a8aa136ca3855d5c1eee0d2
martamalm <144331091+martamalm@users.noreply.github.com>
2024-12-10 13:57:31 +0100

    nu hämtar den bild och header från varje vald carousel_item i wp

--- tr-theme/frontend/src/components/Home/CarouselItem.jsx
+++ tr-theme/frontend/src/components/Home/CarouselItem.jsx
@@ -4,11 +4,14 @@ const CarouselItem = ({ carouselItem }) => {
   return (
     <div className="w-full h-54 bg-white shadow-lg rounded-lg  flex flex-col items-center justify-center transition-transform duration-300 transform hover:scale-105 cursor-pointer">
       <img
-        src={carouselItem}
+        src={carouselItem.acf?.rolemodel_image}
         alt="Carousel Item"
         className="w-full h-54 object-cover rounded-lg"
       />
-<h3>{carouselItem.rolemodel_header}</h3>
+      <h2>
+      {carouselItem.acf?.rolemodel_header|| "Header could not be found"}
+      </h2>
+ 
       
     </div>
     //test kod
