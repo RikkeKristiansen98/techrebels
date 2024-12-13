3d7547745a788bf267e9fb6f4a96972e1fa0759d
martamalm <144331091+martamalm@users.noreply.github.com>
2024-12-13 14:08:05 +0100

    påbörjat styling

--- tr-theme/frontend/src/pages/CollectionItemPage.jsx
+++ tr-theme/frontend/src/pages/CollectionItemPage.jsx
@@ -1,39 +1,34 @@
 export const CollectionItemPage = () => {
   return (
-    <>
-      <div>
-        <div className="flex flex-row items-center w-full min-h-screen">
-          {/* Left Section - Book Cover and Description */}
-          <div className="flex flex-col items-center space-y-8">
-            {/* Book Cover */}
-            <div className="w-48 h-64 bg-gray-300 flex items-center justify-center">
-              <img
-                src="src/images/image1.jpg"
-                alt="Book Cover"
-                className="w-full h-full object-cover"
-              />
-            </div>
-
-            {/* Description */}
-            <div className="w-full bg-gray-200 p-4">
-              <h3 className="text-lg font-bold">Bokens titel igen kanske?</h3>
-              <p className="text-sm mt-2">Beskrivning av boken</p>
-            </div>
-          </div>
-
-          
-          <div className="flex flex-col items-center">
-            {/* Title and Info */}
-            <div className="text-center">
-              <h1 className="text-2xl font-bold">Bokens titel</h1>
-              <p className="text-sm mt-2">
-                Författare: 
-              </p>
-            </div>
-          </div>
-        </div>
+    <div className="grid grid-cols-1 items-center justify-center md:grid-cols-2 gap-8 w-full min-h-screen p-8">
+    {/* Left Section - Book Cover and Description */}
+    <div className="flex flex-col items-center space-y-8">
+      {/* Book Cover */}
+      <div className="w-48 h-64 bg-gray-300 flex items-center justify-center">
+        <img
+          src="src/images/image1.jpg"
+          alt="Book Cover"
+          className="w-full h-full object-cover"
+        />
       </div>
-    </>
+  
+      {/* Description */}
+      <div className="w-full bg-gray-200 p-4 text-center md:text-left">
+        <h3 className="text-lg font-bold">Beskrivning</h3>
+        <p className="text-sm mt-2">Beskrivning av boken</p>
+      </div>
+    </div>
+  
+    {/* Right Section - Title and Info */}
+    <div className="flex flex-col items-center md:items-start justify-start">
+      {/* Title and Info */}
+      <div className="text-center md:text-left">
+        <h1 className="text-2xl font-bold">Tech för alla</h1>
+        <p className="text-sm mt-2">Författare: </p>
+      </div>
+    </div>
+  </div>
+  
   );
 };
 export default CollectionItemPage;
