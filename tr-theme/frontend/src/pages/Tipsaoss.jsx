72b13b518b51925c9c4f787edf75870518a031a6
martamalm <144331091+martamalm@users.noreply.github.com>
2025-01-14 11:22:59 +0100

    småfix med text

--- tr-theme/frontend/src/pages/Tipsaoss.jsx
+++ tr-theme/frontend/src/pages/Tipsaoss.jsx
@@ -226,7 +226,7 @@ const TipsaOss = () => {
                 htmlFor="floating_beskrivning"
                 className="peer-focus:font-medium absolute xl:text-xl xxs:text-sm font-semibold text-gray-700 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
-                Beskriv din idé här:
+                Beskriv ditt tips här:
               </label>
               {errors.beskrivning && (
                 <span className="text-red-500 text-sm">
