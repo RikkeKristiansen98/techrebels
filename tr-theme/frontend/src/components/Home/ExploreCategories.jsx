81659b5dba4a61dc12b64ae2e148eaa5964aa790
Rikke Kristiansen <rikke.kristiansen@hotmail.com>
2025-01-21 13:42:01 +0100

    responsivitet

--- tr-theme/frontend/src/components/Home/ExploreCategories.jsx
+++ tr-theme/frontend/src/components/Home/ExploreCategories.jsx
@@ -3,22 +3,23 @@ import ExploreCategoryButton from "./ExploreCategoryButton";
 
 const ExploreCategories = ({ banner_icons, banner_header, banner_tagline }) => {
   return (
-    <div className="explore-categories flex bg-blueTheme h-96 relative my-20">
-      {/* Text container*/}
-      <div className="text-container h-full flex flex-col justify-center w-1/2 relative overflow-hidden">
-        <div className="explore-categories-text text-white px-20 relative z-10">
-          <h3 className="text-4xl font-bold mb-2">{banner_header}</h3>
-          <p>{banner_tagline}</p>
+    <div className="explore-categories flex bg-blueTheme h-96 relative my-20 flex-col sm:flex-row">
+    {/* Text container */}
+    <div className="text-container h-full flex flex-col justify-center w-full sm:w-1/2 relative overflow-hidden mb-2 sm:mb-0">
+      <div className="explore-categories-text text-white px-5 sm:px-20 relative z-10 sm:block">
+        <h3 className="text-4xl font-bold mb-2">{banner_header}</h3>
+        <p>{banner_tagline}</p>
         </div>
 
         {/* Text container shape */}
-        <div className="absolute top-0 right-0 w-[200%] h-[200%] bg-orangeTheme rounded-full"></div>
-      </div>
+        <div className="absolute top-0 right-0 w-full h-16 bg-orangeTheme sm:w-[200%] sm:h-[200%] sm:rounded-full xxs:hidden xs:hidden sm:block"></div>
+        </div>
+      
 
       {/* Buttons */}
 
-      <div className="explore-categories-buttons flex-1 flex justify-around">
-        {banner_icons.map((icon, index) => (
+      <div className="explore-categories-buttons flex-1 flex justify-around xxs:bg-orangeTheme xs:bg-orangeTheme sm:bg-transparent">
+      {banner_icons.map((icon, index) => (
           <ExploreCategoryButton key={index} icon={icon} />
         ))}
       </div>
