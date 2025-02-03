e47087276baa79de54dc13eec645a124ba254389
martamalm <144331091+martamalm@users.noreply.github.com>
2025-02-03 11:45:42 +0100

    raderat css filen för footer, gjort om till tailwind css, småfix i carousel.jsx

--- tr-theme/frontend/src/components/Footer.jsx
+++ tr-theme/frontend/src/components/Footer.jsx
@@ -1,48 +1,53 @@
 import React from "react";
-import "../styles/components-styles/footer.css";
 import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <div className="newsletter">
-          <h3 className="header-2 text-xl">Prenumerera på nyhetsbrev</h3>
-          <form className="newsletter-container">
+    <footer className="relative text-white overflow-visible">
+      <div className="relative z-20 text-center flex justify-between items-center p-12">
+        <div className="flex flex-col justify-start items-center gap-8 text-center xl:ml-[10%]">
+          <h3 className="text-3xl font-semibold">Prenumerera på nyhetsbrev</h3>
+          <form className="flex justify-center xl:mb-[20%]">
             <input
               type="email"
-              className="input-email"
+              className="w-full p-3 pr-[100px] bg-white text-[20px] border-none outline-none rounded-l-lg"
               id="textInput"
               placeholder="Email..."
             />
-            <button type="submit" className="send-nl-btn">
+            <button
+              type="submit"
+              className="bg-[#FF9B71] text-white text-[25px] font-semibold p-4 rounded-r-lg"
+            >
               Skicka
             </button>
           </form>
         </div>
 
-        <div className="faq xxs:text-sm">
-          <h3>Har du frågor?</h3>
-          <p>
-            I vår{" "}
-            <NavLink to="/faq" className="faq-link">
+        <div className="xl:mr-[10%]">
+          <h3 className="xl:text-3xl xl:mb-[2%]">Har du frågor?</h3>
+          <p className="xl:text-2xl">
+            I vår {" "}
+            <NavLink to="/faq" className="text-[#FF9B71] hover:text-white hover:underline">
               FAQ
             </NavLink>{" "}
             hittar du svaren på de vanligaste frågorna.
           </p>
-          <p className="xxs:m-[5%]">&copy; Tech för alla</p>
+          <p className="mt-5 text-xl">&copy; Tech för alla</p>
         </div>
       </div>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 400"
-        className="footer-svg"
+        className="absolute bottom-0 left-0 w-full h-auto"
       >
         <path
-          fill="#16697A"
+          fill="#996AD9"
           d="M0,150L48,140C96,130,192,80,288,70C384,60,480,90,576,130C672,170,768,220,864,215C960,210,1056,160,1152,135C1248,110,1344,120,1392,125L1440,130L1440,400L0,400Z"
         ></path>
       </svg>
+      <div className="text-center mt-5 text-sm text-white z-10">
+        &copy; Tech för alla
+      </div>
     </footer>
   );
 };
--- tr-theme/frontend/src/components/Home/Carousel.jsx
+++ tr-theme/frontend/src/components/Home/Carousel.jsx
@@ -14,7 +14,7 @@ const Carousel = ({ carouselItems }) => {
   // Funktion för att ändra antal bilder beroende på skärmstorlek
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth <= 375) {
+      if (window.innerWidth <= 430) {
         setItemsPerSlide(1); // Visa en bild vid skärmbredd <= 375px
       } else {
         setItemsPerSlide(3); // Visa 3 bilder annars
@@ -73,10 +73,10 @@ const Carousel = ({ carouselItems }) => {
   return (
     <div className="relative w-full mb-[20%] mt-[5%] flex justify-center items-center">
       {/* Bakgrund med endast opacity på bakgrunden */}
-      <div className="absolute inset-0 bg-orangeTheme opacity-30 z-0"></div>{" "}
+      <div className="absolute inset-0 bg-purpleTheme  z-0"></div>{" "}
       {/* Bakgrund med opacity */}
       {/* Flex container för text och karusell bredvid varandra */}
-      <div  className="flex flex-col-reverse xl:flex-row items-center justify-center w-full space-x-0 xl:space-x-28 mt-[4%] mb-[4%]">
+      <div className="flex flex-col-reverse xl:flex-row items-center justify-center w-full space-x-0 xl:space-x-28 mt-[4%] mb-[1%]">
         {/* Karusellen (vänster sida) */}
         <div className="relative xl:w-[50%] xxs:w-[60%] mr-[5%] xl:mb-[-10%]">
           <div className="relative flex transition-all duration-700 ease-in-out gap-7 justify-center">
@@ -85,7 +85,11 @@ const Carousel = ({ carouselItems }) => {
               className="absolute xl:left-[-5%] xxs:left-[-25%] top-1/2 transform -translate-y-1/2 z-20"
               onClick={prevSlide}
             >
-              <img src={leftArrow} alt="left arrow" className="w-7 h-7" />
+              <img
+                src={leftArrow}
+                alt="left arrow"
+                className="w-7 h-7 invert"
+              />
             </button>
 
             {/* Loopa igenom de synliga korten och rendera dem */}
@@ -102,7 +106,11 @@ const Carousel = ({ carouselItems }) => {
               className="absolute xl:right-[-5%] xxs:right-[-25%] top-1/2 transform -translate-y-1/2 z-20"
               onClick={nextSlide}
             >
-              <img src={rightArrow} alt="right arrow" className="w-7 h-7" />
+              <img
+                src={rightArrow}
+                alt="right arrow"
+                className="w-7 h-7 invert"
+              />
             </button>
           </div>
 
@@ -113,8 +121,8 @@ const Carousel = ({ carouselItems }) => {
                 key={index}
                 className={`w-3 h-3 rounded-full ${
                   currentIndex === index
-                    ? "bg-gray-600" // Aktuell bild
-                    : "bg-gray-400"
+                    ? "bg-gray-700" // Aktuell bild
+                    : "bg-white"
                 }`}
               />
             ))}
@@ -128,10 +136,10 @@ const Carousel = ({ carouselItems }) => {
             isVisible ? "animate-slide-in-right" : "" // Lägg till animation om sektionen är synlig
           } space-y-10`}
         >
-          <h2 className="header-2 xxs:text-2xl xl:text-5xl text-gray-800 xl:mt-[8%] border-b-2 pb-4 mx-auto border-gray-800">
+          <h2 className="header-2 xxs:text-2xl xl:text-5xl text-white xl:mt-[8%] border-b-2 pb-4 mx-auto border-white">
             Tech för alla tipsar
           </h2>
-          <p className="xl:text-2xl xxs:text-lg">
+          <p className="xl:text-2xl xxs:text-lg text-white">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
             voluptate vitae ullam exercitationem iste error velit deleniti. Fuga
             nisi illum reiciendis, magnam necessitatibus facere assumenda
@@ -140,11 +148,9 @@ const Carousel = ({ carouselItems }) => {
           <div className="xl:pt-[5%] xxs:pb-[35%]">
             <NavLink
               to="/collection-page"
-              className="group relative xl:text-3xl xxs:text-xl font-semibold mx-auto border-gray-800 pb-2"
+              className="xl:text-3xl xxs:text-xl font-semibold text-white transition-transform duration-300 hover:scale-110"
             >
               Gå till tipsbanken
-              {/* Strecket under med animation som slide:ar in när man hovrar över */}
-              <span className="absolute bottom-0 left-0 w-0 h-1 bg-gray-800 transition-all duration-300 ease-in-out group-hover:w-full"></span>
             </NavLink>
           </div>
         </div>
