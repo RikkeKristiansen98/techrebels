874fe0a711dcb1d963dba48097cfca0ca3daef3a
varamara <mikahvaarama@gmail.com>
2025-02-11 11:09:38 +0100

    feat: enhance ExploreBanner and PromoSection components with improved layout and image support

--- tr-theme/frontend/src/components/Home/ExploreBanner.jsx
+++ tr-theme/frontend/src/components/Home/ExploreBanner.jsx
@@ -4,22 +4,22 @@ import BannerButton from "./BannerButton";
 const ExploreBanner = ({ banner_icons, banner_header, banner_tagline }) => {
   return (
     <div className="explore-categories flex bg-pinkTheme h-96 relative my-20 flex-col sm:flex-row">
-    {/* Text container */}
-    <div className="text-container h-full flex flex-col justify-center w-full sm:w-1/2 relative overflow-hidden mb-2 sm:mb-0">
-      <div className="explore-categories-text text-white px-5 sm:px-20 relative z-10 sm:block">
-        <h3 className="text-4xl font-bold mb-2">{banner_header}</h3>
-        <p>{banner_tagline}</p>
+      {/* Text container */}
+      <div className="text-container h-full flex flex-col justify-center w-full sm:w-1/2 relative overflow-hidden mb-2 sm:mb-0">
+        <div className="explore-categories-text text-white px-5 sm:px-20 relative z-10 sm:block">
+          <h3 className="text-4xl font-bold mb-2">{banner_header}</h3>
+          <p>{banner_tagline}</p>
         </div>
 
         {/* Text container shape */}
         <div className="absolute top-0 right-0 w-full h-16 bg-purpleTheme sm:w-[200%] sm:h-[200%] sm:rounded-full xxs:hidden xs:hidden sm:block"></div>
-        </div>
-      
+      </div>
+
 
       {/* Buttons */}
 
       <div className="explore-categories-buttons flex-1 flex justify-around xxs:bg-orangeTheme xs:bg-orangeTheme sm:bg-transparent">
-      {banner_icons.map((icon, index) => (
+        {banner_icons.map((icon, index) => (
           <BannerButton key={index} icon={icon} />
         ))}
       </div>
--- tr-theme/frontend/src/components/Home/PromoSection.jsx
+++ tr-theme/frontend/src/components/Home/PromoSection.jsx
@@ -6,38 +6,47 @@ const PromoSection = ({
     promo_description,
     promo_link_title,
     promo_link_url,
+    promo_image_url
 }) => {
+    const promoImage = promo_image_url;
+    console.log("Image URL:", promo_image_url);
+
+
     return (
-        <div className="promo-section flex bg-purpleTheme h-96 relative flex-col sm:flex-row">
+
+        <div className="promo-section flex h-96 bg-purpleTheme  relative flex-col sm:flex-row">
+
             {/* Text container */}
-            <div className="text-container border h-full flex items-center justify-center w-full sm:w-1/2 relative overflow-hidden mb-2 sm:mb-0">
+            <div className="text-container border h-full flex flex-col items-center justify-center w-full sm:w-1/2 relative overflow-hidden mb-2 sm:mb-0">
                 <h2 className="header-1 text-4xl">{promo_header}</h2>
-                <div className="link-description">{promo_description}</div>
-                <div className="link-text mt-[7%] flex items-center justify-center text-center z-20 group">
-                    <Link
-                        className="header-2 flex items-center text-4xl transition-transform duration-300 ease-in-out group-hover:translate-x-2"
-                        to={promo_link_url}
-                    >
-                        {promo_link_title}
-                        <svg
-                            className="ml-2 mt-1"
-                            fill="#FFFFFF"
-                            height="25px"
-                            version="1.1"
-                            id="Layer_1"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 330 330"
-                        >
-                            <path
-                                id="XMLID_222_"
-                                d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001 c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213 C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606 C255,161.018,253.42,157.202,250.606,154.389z"
-                            ></path>
-                        </svg>
-                    </Link>
-                </div>
+                <div className="link-description flex">{promo_description}</div>
             </div>
-            {/*shape */}
-            <div className="absolute top-0 right-0 w-full h-16 bg-blueTheme sm:w-[200%] sm:h-[200%] sm:rounded-full xxs:hidden xs:hidden sm:block"></div>
+            {/* image link conatiner  */}
+            <img
+                src={promoImage}
+                alt="Image of browser"
+                className="absolute z-10 right-[15%] top-[15%] scale-[0.7] object-cover"
+            />
+            <Link
+                className="header-2 flex items-center text-4xl transition-transform duration-300 ease-in-out group-hover:translate-x-2"
+                to={promo_link_url}
+            >
+                {promo_link_title}
+                <svg
+                    className="ml-2 mt-1"
+                    fill="#FFFFFF"
+                    height="25px"
+                    version="1.1"
+                    id="Layer_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 330 330"
+                >
+                    <path
+                        id="XMLID_222_"
+                        d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001 c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213 C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606 C255,161.018,253.42,157.202,250.606,154.389z"
+                    ></path>
+                </svg>
+            </Link>
         </div>
     );
 };
