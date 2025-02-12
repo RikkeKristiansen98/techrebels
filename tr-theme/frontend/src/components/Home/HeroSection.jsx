d72c2ea9138f6c7199b34c385be6e44a9e60b6ea
varamara <mikahvaarama@gmail.com>
2025-02-12 10:49:25 +0100

    feat: update HeroSection with new images, layout adjustments, and improved styling

--- tr-theme/frontend/src/components/Home/HeroSection.jsx
+++ tr-theme/frontend/src/components/Home/HeroSection.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -8,108 +7,54 @@ const HeroSection = ({
   hero_link_title,
   hero_link_url,
 }) => {
-  const heroImage = "https://techforalla.se/images/hero-image.png";
+  const heroImage = "http://techforalla.se/wp-content/uploads/2025/02/robot-1-e1739352899259.png";
+  const heroHeader = "http://techforalla.se/wp-content/uploads/2025/02/techforallaheader-e1739352811594.png";
 
-  return ( 
-    <div className="hero-section relative py-[1%] flex flex-col md:flex-row items-stretch min-h-[600px] overflow-visible">
-      {/* wave element. */}
-      <div className="absolute top-0 left-0 w-full h-[600px] sm:h-[700px] md:h-[800px] overflow-hidden z-0">
-        <svg
-          width="100%"
-          height="100%"
-          viewBox="0 0 1440 1000"
-          preserveAspectRatio="none"
-          className="absolute top-0 left-0 w-full h-full"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M 0,1000 L 0,262 C 71.35885167464113,203.96172248803828 142.71770334928226,145.92344497607655 248,153 C 353.28229665071774,160.07655502392345 492.48803827751203,232.26794258373207 609,306 C 725.511961722488,379.73205741626793 819.3301435406698,455.00478468899524 903,462 C 986.6698564593302,468.99521531100476 1060.1913875598086,407.7129186602871 1148,413 C 1235.8086124401914,418.2870813397129 1337.9043062200958,490.14354066985646 1440,762 L 1440,1000 L 0,1000 Z"
-            stroke="none"
-            strokeWidth="0"
-            fill="#996ad9"
-            fillOpacity="1"
-          />
-        </svg>
-      </div>
-
-    {/* DEN GAMLA / lila SVG wave)
- <div className="hero-section relative py-[3%] flex flex-col md:flex-row items-stretch min-h-[250px] overflow-visible">
-      {/* wave element 
-      <div className="absolute top-0 left-0 w-full h-full z-0">
-  <svg
-    width="100%"
-    height="100%"
-    id="svg-wave"
-    viewBox="0 0 1440 690"
-    className="absolute top-0 left-0 w-full h-full scale-[1.5] 
-               xxs:scale-[2.8] xxs:top-[1%] 
-               xs:scale-[2.1] xs:top-[5%] 
-               sm:scale-[1] sm:top-[0%] 
-               md:scale-[2.7] md:top-[0%]"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M 0,700 L 0,262 C 71.35885167464113,203.96172248803828 142.71770334928226,145.92344497607655 248,153 C 353.28229665071774,160.07655502392345 492.48803827751203,232.26794258373207 609,306 C 725.511961722488,379.73205741626793 819.3301435406698,455.00478468899524 903,462 C 986.6698564593302,468.99521531100476 1060.1913875598086,407.7129186602871 1148,413 C 1235.8086124401914,418.2870813397129 1337.9043062200958,490.14354066985646 1440,562 L 1440,700 L 0,700 Z"
-      stroke="none"
-      strokeWidth="0"
-      fill="#996ad9"
-      fillOpacity="1"
-    ></path>
-  </svg>
-</div> */}
-
-    {/*  GAMLA hero text 
-      <div className="hero-section-text mt-[10%] px-[8%] relative flex-1 flex flex-col items-start text-center md:text-left">
-        <h1 className="hero-title text-white text-5xl font-bold mt-[20%] animate-slide-in-left">
-          {hero_header}
-        </h1>
-        <p className="intro-text text-white text-lg animate-slide-in-left mt-[5%]">
-          {hero_description}
-        </p>
-*/}
-      {/* hero text */}
-      <div className="hero-section-text
-                xxs:mt-[20%] xs:mt-[35%] sm:mt-[8%]
-                px-[7%] relative flex-1 flex flex-col 
-                items-start md:text-left md:mt-[20%] xl:mt-[8%] xl:text-4xl">
-  <h1 className="hero-title text-white font-bold animate-slide-in-left 
-                 xxs:text-2xl xs:text-2xl sm:text-4xl md:text-3xl md:mt-[15%]
-                 xxs:mt-[60%] xs:mt-[25%] sm:mt-[20%]">
-    {hero_header}
-  </h1>
-  <p className="intro-text text-white animate-slide-in-left 
-  md:text-xl
+  return (
+    <div className="hero-section bg-pinkTheme relative py-[6%] px-[10%] flex flex-row min-h-[600px] overflow-visible">
+      {/* Left container */}
+      <div className="left flex-1 flex flex-col items-start justify-center ">
+        <img
+          src={heroHeader}
+          alt="Image of browser"
+          className="z-10 scale-[0.8] object-cover"
+        />
+        {/* Hero text */}
+        <div className="hero-section-text flex flex-col items-center">
+          {/* <h1 className="hero-title text-white font-bold animate-slide-in-left 
+                 xxs:text-2xl xs:text-2xl sm:text-4xl md:text-3xl">
+            {hero_header}
+          </h1> */}
+          <p className="text-center intro-text text-blackTheme animate-slide-in-left px-[5%]
+                md:text-xl
                 xxs:text-sm xs:text-base sm:text-lg
                 xxs:mt-[5%] xs:mt-[10%] sm:mt-[5%]">
-    {hero_description}
-  </p>
-        {/* hero link */}
-        <div className="hero-link-text mt-[7%] flex items-center justify-center text-center z-20 group">
-          <Link
-            className="header-2 flex items-center text-4xl transition-transform duration-300 ease-in-out group-hover:translate-x-2"
-            to={hero_link_url}
-          >
-            {hero_link_title}
-            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1" />
-            </svg>
-          </Link>
+            {hero_description}
+          </p>
         </div>
       </div>
 
-      {/* hero image */}
-      <div className="hero-section-image flex-1 relative flex items-center justify-center overflow-visible
-                xl:left-[23%] xs:absolute xs:top-[3%] xs:scale-[0.6] xs:w-full xs:h-auto
-                xxs:absolute xs:top-[-0%] xxs:left-[18%] xxs:w-full xxs:h-auto">
-  <img
-    src={heroImage}
-    alt="Image of browser"
-    className="absolute z-1+ left-[20%] top-[15%] scale-[0.5] object-cover 
-               md:mt-[-9%] xxs:top-[-5%] xxs:left-[-1%] xxs:scale-[0.6]" 
-  />
-</div>
+      {/* Right container */}
+      <div className="right flex-1 flex flex-col items-center justify-center">
+        <div className="hero-section-image relative flex items-center justify-center overflow-visible">
+          <img
+            src={heroImage}
+            alt="Image of browser"
+            className="scale-[0.7] object-cover"
+          />
+        </div>
+        {/* Hero link */}
+        <div className="hero-link-text flex items-center justify-center text-center z-20 group">
+          <Link
+            className="bg-orange-500 border-blackTheme border-2 shadow-[4px_4px_3px_rgba(0,0,0,0.6)] rounded-lg px-4 py-2 flex items-center text-blackTheme text-xl font-bold transition-transform duration-200 ease-in-out hover:scale-95 active:scale-90"
+            to={hero_link_url}
+          >
+            {hero_link_title}
+          </Link>
+        </div>
 
-    </div> 
+      </div>
+    </div>
   );
 };
 
