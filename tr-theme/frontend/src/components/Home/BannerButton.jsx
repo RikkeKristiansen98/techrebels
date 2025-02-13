22bc44c147b49f3229ee3db9f2f18803733f40d6
varamara <mikahvaarama@gmail.com>
2025-02-13 13:12:53 +0100

    Increase icon scale and adjust margin in BannerButton; fix spacing in ExploreBanner icon container

--- tr-theme/frontend/src/components/Home/BannerButton.jsx
+++ tr-theme/frontend/src/components/Home/BannerButton.jsx
@@ -9,7 +9,7 @@ const BannerButton = ({ icon }) => {
           <img
             src={icon.icon}
             alt={icon.title}
-            className="scale-[2] w-16 h-16 object-contain mb-8 xxs:w-10 xxs:h-10 xs:w-12 xs:h-12 sm:w-12 sm:h-12"
+            className="scale-[3] w-16 h-16 object-contain mb-14 xxs:w-10 xxs:h-10 xs:w-12 xs:h-12 sm:w-12 sm:h-12"
           />
           {/* Title */}
           <p className="text-2xl font-semibold text-blackTheme xxs:text-xs xs:text-sm sm:text-lg">
