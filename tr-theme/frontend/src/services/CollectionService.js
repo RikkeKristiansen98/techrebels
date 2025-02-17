d2de20510bdf23b8d9ecd04ebaab5869c8c5bedc
martamalm <144331091+martamalm@users.noreply.github.com>
2025-02-17 13:06:38 +0100

    nytt felmeddeande i collectionservice

--- tr-theme/frontend/src/services/CollectionService.js
+++ tr-theme/frontend/src/services/CollectionService.js
@@ -4,23 +4,23 @@ const CollectionService = {
   async fetchCollection(page = 1, perPage = 16) {
     try {
       const response = await fetch(`${BASE_URL}/collection?page=${page}&per_page=${perPage}`);
-
+      
       if (!response.ok) {
+        // Logga felet och svar från servern
+        console.error(`HTTP-fel! status: ${response.status}`, await response.text());
         throw new Error(`HTTP-fel! status: ${response.status}`);
       }
-      const data = await response.json();
 
-      return data.map((item) => {
-        return {
-          id: item.id, 
-          title: item.acf?.collection_item_title || "No Title", 
-          tagline: item.acf?.collection_item_tagline || "No Tagline", 
-          description: item.acf?.collection_item_description || "No description",
-          categories: item.categories || [], 
-          imageSrc: item.acf?.collection_item_image || null, 
-          url: item.acf?.collection_item_url || "#", 
-        };
-      });
+      const data = await response.json();
+      return data.map((item) => ({
+        id: item.id,
+        title: item.acf?.collection_item_title || "No Title",
+        tagline: item.acf?.collection_item_tagline || "No Tagline",
+        description: item.acf?.collection_item_description || "No description",
+        categories: item.categories || [],
+        imageSrc: item.acf?.collection_item_image || null,
+        url: item.acf?.collection_item_url || "#",
+      }));
     } catch (error) {
       console.error("Error fetching collection:", error);
       throw error;
