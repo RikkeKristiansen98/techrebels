1d84328200a9f8641b4ae4476fcb6f8e6084e8b5
Rikke Kristiansen <rikke.kristiansen@hotmail.com>
2025-02-19 10:42:58 +0100

    idk

--- tr-theme/functions.php
+++ tr-theme/functions.php
@@ -9,14 +9,43 @@ function load_react_app() {
 add_action('wp_enqueue_scripts', 'load_react_app');
 add_filter('acf/rest_api/key', '__return_true');
 
-// Registrera en ny REST API-slutpunkt
-add_action('rest_api_init', function () {
-    register_rest_route('my-api/v1', '/send-email/', [
-        'methods' => 'POST',
-        'callback' => 'send_email_callback',
-        'permission_callback' => '__return_true', // Säkerställ att alla kan använda den här slutpunkten
-    ]);
-});
+function send_email_callback(WP_REST_Request $request) {
+    $data = $request->get_json_params();
+
+    // Hämta data från formuläret
+    $email = sanitize_email($data['email']);
+    $kategori = sanitize_text_field($data['kategori']);
+    $titel = sanitize_text_field($data['titel']);
+    $alder = sanitize_text_field($data['alder']);
+    $beskrivning = sanitize_textarea_field($data['beskrivning']);
+
+    // Säkerhetskontroll
+    if (empty($email) || empty($kategori) || empty($titel) || empty($alder) || empty($beskrivning)) {
+        return new WP_REST_Response(['message' => 'Alla fält måste fyllas i'], 400);
+    }
+
+    // Mottagarens e-post
+    $to = "hello@genzconsulting.se"; 
+    $subject = "Nytt tips inskickat: " . $titel;
+    $headers = array('Content-Type: text/html; charset=UTF-8', 'From: ' . $email);
+
+    // Meddelandets innehåll
+    $message = "<h2>Nytt tips inskickat</h2>";
+    $message .= "<p><strong>E-post:</strong> {$email}</p>";
+    $message .= "<p><strong>Kategori:</strong> {$kategori}</p>";
+    $message .= "<p><strong>Titel:</strong> {$titel}</p>";
+    $message .= "<p><strong>Ålder:</strong> {$alder}</p>";
+    $message .= "<p><strong>Beskrivning:</strong> {$beskrivning}</p>";
+
+    // Skicka e-post
+    $sent = wp_mail($to, $subject, $message, $headers);
+
+    if ($sent) {
+        return new WP_REST_Response(['message' => 'E-post skickad!'], 200);
+    } else {
+        return new WP_REST_Response(['message' => 'E-post kunde inte skickas.'], 500);
+    }
+}
 
 function expose_acf_fields_for_pages() {
     // Lägg till ACF-fält till REST API för alla sidor
