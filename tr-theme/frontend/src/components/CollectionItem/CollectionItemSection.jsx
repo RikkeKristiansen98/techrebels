ea55d2530eb9148ef7bcb59e48f8280c6cf8ce74
varamara <mikahvaarama@gmail.com>
2025-02-24 14:43:11 +0100

    klar med styling collectionitempage

--- tr-theme/frontend/src/components/CollectionItem/CollectionItemSection.jsx
+++ tr-theme/frontend/src/components/CollectionItem/CollectionItemSection.jsx
@@ -2,7 +2,7 @@ export const CollectionItemSection = () => {
     const borderElement = "http://techforalla.se/wp-content/uploads/2025/02/Tech-Rebels-sidelement-1-e1740393062433.png";
 
     return (
-        <div className="bg-pinkTheme relative flex flex-col justify-center items-center h-auto w-full lg:flex-row lg:items-start lg:justify-between py-10 md:py-[10%] lg:py-[8%] lg:px-[12%]">
+        <div className="bg-pinkTheme relative flex flex-col justify-center items-center h-auto w-full xxs:pt-20 xxs:px-3 lg:flex-row lg:items-start lg:justify-between py-10 md:py-[10%] lg:py-[9%] lg:px-[12%]">
             {/* Border element top */}
             <img
                 src={borderElement}
@@ -15,14 +15,14 @@ export const CollectionItemSection = () => {
                 <img
                     src="src/images/bookOne.png"
                     alt="Book Cover"
-                    className="h-[500px] object-cover relative z-10 overflow-visible"
+                    className="lg:h-[500px] xxs:h-[400px] object-cover relative z-10 overflow-visible"
                 />
             </div>
 
             {/* Text container */}
             <div className="relative w-full h-auto flex flex-col justify-center py-16 lg:w-1/2 lg:px-5">
                 {/* Title and Author */}
-                <div className="flex flex-col justify-center pr-5 mb-6 lg:mb-0">
+                <div className="flex flex-col justify-center pr-5 mb-6 xxs:items-center lg:items-start lg:mb-0">
                     <h1 className="text-5xl md:text-6xl text-whiteTheme font-bold mb-5 drop-shadow-lg">Book Title</h1>
                     <p className="text-xl md:text-3xl text-whiteTheme">John Doe</p>
                 </div>
