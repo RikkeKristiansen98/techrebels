d3482a0c3bba7d6779d102b3115652205313a81c
varamara <mikahvaarama@gmail.com>
2025-02-25 14:53:12 +0100

    Rename comment for image in CollectionItemSection and remove unused borderElement in CollectionItemPage

--- tr-theme/frontend/src/pages/CollectionItemPage.jsx
+++ tr-theme/frontend/src/pages/CollectionItemPage.jsx
@@ -2,8 +2,6 @@ import CollectionItemSection from "../components/CollectionItem/CollectionItemSe
 
 export const CollectionItemPage = () => {
 
-  const borderElement = "http://techforalla.se/wp-content/uploads/2025/02/Tech-Rebels-sidelement-1-e1740393062433.png";
-
   return (
     <div className="bg-yellowTheme flex flex-col items-center bg-orangeTheme h-auto lg:flex-row lg:items-start lg:justify-between py-10 md:py-[10%] lg:py-[7%] lg:pb-[10%]">
       <CollectionItemSection />
